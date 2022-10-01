88a3ccead87e8d2df7bb4b99979e014c381073b5
Connor Henderson <connorhenderson20@gmail.com>
2022-10-01 02:59:50 -0400

    Finished product

--- functions.py
+++ functions.py
@@ -2,6 +2,7 @@ import meteor_data_class
 
 mass_list = []
 year_list = []
+meteor_list = []
 
 
 def create_list(file_name):
@@ -11,20 +12,62 @@ def create_list(file_name):
             line = str(line)
             new_line = line.strip()
             line_list = new_line.split('\t')
-            meteor_data_obj = check_mass(line_list)
-            mass_list.append(meteor_data_obj)
+            while len(line_list) <= 11:
+                line_list.append(None)
+            create_meteor_obj(line_list)
+
+        for meteor in meteor_list:
+            check_mass(meteor)
+            check_year(meteor)
+
+        print_results()
 
 
-def check_mass(line_list):
-    if line_list[4].isnumeric() and int(line_list[4]) > 29000000:
-        meteor_data_obj = meteor_data_class.MeteorDataEntry(line_list[0], line_list[1], line_list[2], line_list[3],
-                                                            line_list[4], line_list[5], line_list[6], line_list[7],
-                                                            line_list[8], line_list[9], line_list[10], line_list[11])
-        return meteor_data_obj
-    else:
-        return
+def create_meteor_obj(line_list):
+    meteor_data_obj = meteor_data_class.MeteorDataEntry()
+    meteor_data_obj.set_name(line_list[0])
+    meteor_data_obj.set_id(line_list[1])
+    meteor_data_obj.set_nametype(line_list[2])
+    meteor_data_obj.set_recclass(line_list[3])
+    meteor_data_obj.set_mass(line_list[4])
+    meteor_data_obj.set_fall(line_list[5])
+    meteor_data_obj.set_year(line_list[6])
+    meteor_data_obj.set_reclat(line_list[7])
+    meteor_data_obj.set_reclong(line_list[8])
+    meteor_data_obj.set_geoloc(line_list[9])
+    meteor_data_obj.set_states(line_list[10])
+    meteor_data_obj.set_counties(line_list[11])
+    meteor_list.append(meteor_data_obj)
 
 
-def check_year():
-    return "hi"
+def check_mass(meteor):
+    mass = meteor.mass
+    if mass.isnumeric() and int(mass) >= 2900000:
+        mass_list.append(meteor)
 
+
+def check_year(meteor):
+    year = meteor.year
+    if year is not None and year.isnumeric() and int(meteor.year) >= 2013:
+        year_list.append(meteor)
+
+def print_results():
+    name = 'Name'
+    mass = 'Mass (g)'
+    print(f'{name:<24}{mass:<20}')
+    print('=' * 34)
+    for meteor in mass_list:
+        name_label = meteor.name
+        mass_label = meteor.mass
+        print(f'{name_label:<24}{mass_label:<20}')
+
+    print('\n')
+
+    name = 'Name'
+    mass = 'Year'
+    print(f'{name:<24}{mass:<20}')
+    print('=' * 34)
+    for meteor in year_list:
+        name_label = meteor.name
+        year_label = meteor.year
+        print(f'{name_label:<24}{year_label:<20}')
